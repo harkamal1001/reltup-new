08ff870e1bcd39fe2dc60b1d50fa76a42939b397
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 17:39:13 +0530

    login user

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -33,6 +33,36 @@ angular.module('mainCtrl', [])
 			});	
 	});	
 
+$scope.submitLoginForm = function() {
+            $scope.processing = true;
+
+            // clear the error
+            $scope.error = '';
+            //check to make sure the form is completely valid
+            if ($scope.LoginForm.$valid) {
+                alert("Valid login form");
+                $scope.processing = true;
+
+                // clear the error
+                $scope.error = '';
+
+                Auth.login($scope.LoginForm.validUsername, $scope.LoginForm.validPassword)
+                    .success(function(data) {
+                        $scope.processing = false;			
+
+                        // if a user successfully logs in, redirect to users page
+                        if (data.success)			
+                            $location.path('/users');
+                        else 
+                            $scope.error = data.message;
+
+                    });
+     
+            }
+        }; // Login form #ends
+
+
+
 	// function to handle login form
 	vm.doLogin = function() {
 		vm.processing = true;
