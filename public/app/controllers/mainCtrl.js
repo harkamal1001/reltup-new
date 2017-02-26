83f527cc82eef8c794419f611b34db63b6aa6e48
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-26 18:40:55 +0530

    login
    123

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -46,10 +46,12 @@ vm.processing = true;
 				vm.processing = false;			
 
 				// if a user successfully logs in, redirect to users page
-				if (data.success){			
+				if (data.success){	
+				document.getElementsByClassName("modal-backdrop").className =
+   						document.getElementsByClassName("modal-backdrop").className.replace
+      ( /(?:^|\s)in(?!\S)/g , 'out' );		
 					$location.path('/thankyou');
-				document.getElementsByClassName("modal-backdrop");
-
+				
 					 //angular.element( document.querySelector( '.modal-backdrop.fade.in' ) ).css('display','none');
 				}else 
 					$scope = data.message;
