3ee41a7285bb2fee77529473ceef90e3448d5738
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-27 00:10:26 +0530

    create

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -85,4 +85,23 @@ vm.submitRegForm = function() {
 	};		
 	
 
+	
+vm.type = 'create';
+
+	// function to create a user
+	vm.saveUser = function() {
+		vm.processing = true;
+		vm.message = '';
+
+		// use the create function in the userService
+		User.create(vm.userData)
+			.success(function(data) {
+				vm.processing = false;
+				vm.userData = {};
+				vm.message = data.message;
+			});
+			
+	};		
+	
+	
 });
