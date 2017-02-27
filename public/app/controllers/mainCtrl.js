d20682b54ecf259923757f6b47e0729138922a21
Harkamal Singh <harkamalsingh8@outlook.com>
2017-02-27 11:05:31 +0530

    kkk

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -41,6 +41,8 @@ vm.processing = true;
 		// clear the error
 		vm.error = '';
 
+		console.log("I am checking login");
+
 		Auth.login($scope.validUsername, $scope.validPassword)
 			.success(function(data) {
 				vm.processing = false;			
